73f8abc23f8448eeefc134f968f473950a24e1d0
mennyt <mennyt11@gmail.com>
2021-04-21 15:03:15 +0530

    top module watchdog_top.v added

---
+++ SaCriWaB verilog/watchdog_top.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.04.2021 14:35:41
+// Design Name: 
+// Module Name: watchdog_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module watchdog_top(
+    input [1:0] ABUS,
+    input [7:0] DBUS,
+    input RST,
+    output RSTOUT
+    );
+endmodule
