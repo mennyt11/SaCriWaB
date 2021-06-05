19c0aa89f48f15b48e0c494bce5c1d11e8e7bab8
Jewel Joseph <31788666+je101el@users.noreply.github.com>
2021-06-05 19:55:51 +0530

    Update Brownout_top.v

--- SaCriWaB verilog/Brownout_top.v
+++ SaCriWaB verilog/Brownout_top.v
@@ -1,11 +1,24 @@
 `timescale 1ns / 1ps
 module Brownout_top(
-    input [7:0] ADCin_tp,
     input clk_tp,
+    input [7:0] ADCin_tp, supply_vol, min_vol,
     output Brownout_tp
     );
     wire BODto_rate;
-    BOD_Comparator BOD(.in_bus(ADCin_tp), .BOD_out(BODto_rate));
+    reg [7:0] BOD_value, rateLimit;
+    initial
+    begin
+        BOD_value = 0;
+        rateLimit = 0;
+    end
+    
+    always @(supply_vol or min_vol)
+    begin
+        BOD_value <= (supply_vol + min_vol)/2;
+        rateLimit <= (supply_vol - min_vol)/8;
+    end
+    
+    BOD_Comparator BOD(.in_bus(ADCin_tp), .BOD_out(BODto_rate), .BODvalue(BOD_value));
     Rate_Calculator RateCalc(.clk(clk_tp), .in_bus(ADCin_tp), .BOD_in(BODto_rate), 
-    .Brownout(Brownout_tp));
+    .Brownout(Brownout_tp), .rate_limit(rateLimit));
 endmodule
