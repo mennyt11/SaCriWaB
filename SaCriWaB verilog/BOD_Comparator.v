f72b746c747cae87a6b083cee69f75888e0cda5c
Jewel Joseph <31788666+je101el@users.noreply.github.com>
2021-06-05 19:58:00 +0530

    Update BOD_Comparator.v

--- SaCriWaB verilog/BOD_Comparator.v
+++ SaCriWaB verilog/BOD_Comparator.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 module BOD_Comparator(
-    input [7:0] in_bus,
+    input [7:0] in_bus, BODvalue,
     output reg BOD_out
     );
     always @(in_bus)
     begin
-        if (in_bus<228)
+        if (in_bus<BODvalue)
             BOD_out<=1;
         else
             BOD_out<=0;
