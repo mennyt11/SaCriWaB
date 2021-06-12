eb866de525d20390bace0cec7cdb9d08f497e3e4
annzack <56203303+annzack@users.noreply.github.com>
2021-06-12 22:59:20 +0530

    top tb2 modofied

--- SaCriWaB verilog/testbenches/watchdog_top_tb2.v
+++ SaCriWaB verilog/testbenches/watchdog_top_tb2.v
@@ -23,18 +23,21 @@
 module watchdog_top_tb2();
 
 localparam T = 20;
-reg[1:0] abus;
-reg[7:0] dbus;
+reg[2:0] abus;
+reg[15:0] dbus;
+reg[19:0] adc_in;
 reg clk, rst;
 wire rstout;
 wire WDFAIL;
-wire FLSTAT;
+wire [2:0] FLSTAT;
+wire brownout;
 
-watchdog_top DUT2( .ABUS(abus), .DBUS(dbus), .RST(rst), .CLK(clk), .RSTOUT(rstout), .WDFAIL(WDFAIL), .FLSTAT(FLSTAT));
+watchdog_top DUT2( .ABUS(abus), .DBUS(dbus), .ADC_IN(adc_in), .RST(rst), .CLK(clk), .RSTOUT(rstout), .WDFAIL(WDFAIL), .FLSTAT(FLSTAT), .BROWNOUT(brownout));
  initial
   begin 
   abus = 0;
   dbus = 0;
+  adc_in =20'hFFFF;
   rst = 0;
  end 
  
@@ -45,91 +48,116 @@ watchdog_top DUT2( .ABUS(abus), .DBUS(dbus), .RST(rst), .CLK(clk), .RSTOUT(rstou
  clk = 1'b0;
  #(T/2);
  end 
+ 
  initial
  begin
  @(negedge clk)  //random value
- #5 dbus =8'h36;
-    abus =2'b00;
+ #5 dbus =16'h3636;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // random value
- #5 dbus =8'h11;
-    abus =2'b00;
+ #5 dbus =16'h1111;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // first of the pattern
- #5 dbus =8'hAA;
-    abus =2'b00;
+ #5 dbus =16'hAAAA;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)   // second of the pattern
  @(negedge clk)
  @(negedge clk)
- #5 dbus =8'h55;
-    abus =2'b00;
+ #5 dbus =16'h5555;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)    //(1) write is enabled for next four cycles
- #5 dbus =8'h0A;   // frame window length is given as hex 0A
-    abus =2'b00;
+ #5 dbus =16'h000A;   // frame window length is given as hex 0A
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)
- #5 dbus =8'h03;   // (2)service window length is given as hex 03
-    abus =2'b01;
+ #5 dbus =16'h0003;   // (2)service window length is given as hex 03
+    abus =3'b001;
+    adc_in =20'hFFFFF; 
  @(negedge clk)
- #5 dbus =8'h04;   //(3) reset limit which is the max value of downcounter is given as hex 04
-    abus =2'b11;
+ #5 dbus =16'h0004;   //(3) reset limit which is the max value of downcounter is given as hex 04
+    abus =3'b011;
+    adc_in =20'hFFFFF; 
  @(negedge clk)
- #5 dbus =8'h00;   // (4)zero value to fill write cycle
-    abus =2'b10;
+ #5 dbus =16'h0000;   // (4)zero value to fill write cycle
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  //random value
- #5 dbus =8'h67;
-    abus =2'b10;
+ #5 dbus =16'h6767;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // random value
- #5 dbus =8'h45;
-    abus =2'b10;
+ #5 dbus =16'h4545;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // first of the pattern
- #5 dbus =8'hAA;
-    abus =2'b00;
+ #5 dbus =16'hAAAA;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)   // second of the pattern
  @(negedge clk)
  @(negedge clk)
- #5 dbus =8'h55;
-    abus =2'b00;
+ #5 dbus =16'h5555;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // (1)init signal is given
- #5 dbus =8'h08;
-    abus =2'b10;
+ #5 dbus =16'h0010;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk) // (2)0 value so that system counter keeps counting
  @(negedge clk) //(3)
  @(negedge clk) //(4)
- #5 dbus =8'h00;
-    abus =2'b10;
+ #5 dbus =16'h0000;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // first of the pattern
- #5 dbus =8'hAA;
-    abus =2'b00;
+ #5 dbus =16'hAAAA;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)   // second of the pattern
  @(negedge clk)
  @(negedge clk)
- #5 dbus =8'h55;
-    abus =2'b00;
+ #5 dbus =16'h5555;
+    abus =3'b000;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // (1)watchdog service given ie counter is kicked, service limit goes to high
- #5 dbus =8'h04;
-    abus =2'b10;
+ #5 dbus =16'h0008;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  //(2) random value
- #5 dbus =8'h00;
-    abus =2'b10;
+ #5 dbus =16'h0000;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)   // (3)watchdog service is again given since service limit is already high this is  a fail, flstat = 10;
- #5 dbus =8'h04;
-    abus =2'b10;
+ #5 dbus =16'h0008;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)   ; //(4) time for downcounter, and system to save current data before reset
- #5 dbus =8'h00;
-    abus =2'b10; 
+ #5 dbus =16'h0000;
+    abus =3'b010;
+    adc_in =20'hFFFFF;  
  @(negedge clk)
  @(negedge clk)
  @(negedge clk)
- #5 dbus =8'h00;
-    abus =2'b10;
+ #5 dbus =16'h0000;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // random value
  @(negedge clk)
- #5 dbus =8'h00;
-    abus =2'b10;
+ #5 dbus =16'h0000;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  //random value
- #5 dbus =8'h15;
-    abus =2'b10;
+ #5 dbus =16'h1515;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  @(negedge clk)  // random value
- #5 dbus =8'h71;
-    abus =2'b10;
+ #5 dbus =16'h7171;
+    abus =3'b010;
+    adc_in =20'hFFFFF; 
  end
  
 endmodule
